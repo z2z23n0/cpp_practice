d908811325472ec695a5fcf0f7665677ab92c89a
YuzeZhang <zhangyuzechn@foxmail.com>
2019-10-11 23:45:35 +0800

    commit

---
+++ 002_in&out.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Zeno on 2019/10/27.
+//
+
+#include <iostream>
+using namespace std;
+//int main(){
+//    //cout<<"Hello world"<<endl;
+//    cout<<"Hello world"<<endl;
+//    return 0;
+//}
+int main(){
+    int a;
+    double b;
+    char c;
+    cin>>a;
+    cin>>b>>c;
+
+    cout<<a<<endl;
+    cout<<b<<" "<<c<<endl;
+    return 0;
+}
