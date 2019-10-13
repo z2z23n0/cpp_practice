50ab6233094f08f902d67e3cc8d289b43c5836d8
YuzeZhang <zhangyuzechn@foxmail.com>
2019-10-13 14:36:35 +0800

    commit

---
+++ 004_overloaded_function.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Zeno on 2019/10/27.
+//
+
+#include <iostream>
+using namespace std;
+//int Add(int left,int right){
+//    return left + right;
+//}
+//double Add(double left, double right){
+//    return left + right;
+//}
+//long Add(long left ,long right){
+//    return left + right;
+//}
+//int main(){
+//    double ret = Add(2.2,3.0);
+//    cout<<ret<<endl;
+//    cout<<Add(10,20)<<endl;
+//    cout<<Add(10.1,20.0)<<endl;
+//    cout<<Add(10L,20L)<<endl;
+//}
+//short Add(short left, short right){
+//    return left + right;
+//}
+//int Add(short left, short right){
+//    return left + right;
+//}
+
+void TestFunc(int a = 10){
+    cout<<"void TestFunc(int)"<<endl;
+}
+void TestFunc(int a){
+    cout<<"void TestFunc(int)"<<endl;
+}
