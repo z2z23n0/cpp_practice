d8c7eee5536598dc4f660ceffeffbd144821a749
YuzeZhang <zhangyuzechn@foxmail.com>
2019-10-18 19:22:39 +0800

    commit

---
+++ 008_inline.h
@@ -0,0 +1,16 @@
+//
+// Created by Zeno on 2019/10/27.
+//
+
+#ifndef CPP_PROJECT_008_INLINE_H
+#define CPP_PROJECT_008_INLINE_H
+
+#endif //CPP_PROJECT_008_INLINE_H
+
+#include <iostream>
+using namespace std;
+
+inline void f(int i);
+void f(int i){
+    cout<<i<<endl;
+}
