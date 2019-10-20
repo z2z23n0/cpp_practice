2ef91d981b8e383496703acd140bd82283724607
YuzeZhang <zhangyuzechn@foxmail.com>
2019-10-20 20:25:39 +0800

    commit

---
+++ 010_auto2.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Zeno on 2019/10/27.
+//
+
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+int main(){
+    //int x = 10;
+    //auto a = &x;
+    //auto* b = &x;
+    //auto& c = x;
+    //cout<<typeid(a)<<endl;
+    auto a = 1,b = 2;
+    auto c = 3,d = 3.0;
+}
