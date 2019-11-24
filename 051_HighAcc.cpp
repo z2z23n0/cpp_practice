0761f69be5c8fc47cc889183a0987849089751d5
YuzeZhang <zhangyuzechn@foxmail.com>
2019-11-24 20:48:03 +0800

    change --2019/11/24

--- 051_HighAcc.cpp
+++ 051_HighAcc.cpp
@@ -51,10 +51,12 @@ public:
     }
 };
 int main(){
-    string s1("12345");
-    string s2("98765");
+    string s1;
+    string s2;
+    while(cin>>s1>>s2){
     HighAcc num1(s1);
     HighAcc num2(s2);
     cout<<(string)(num1+num2);
+    }
     return 0;
 }
