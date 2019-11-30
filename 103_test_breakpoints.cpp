d73b8270543614a15e32a01f1c43c7ba804ff2e0
YuzeZhang <zhangyuzechn@foxmail.com>
2019-11-30 14:12:40 +0800

    add --2019/11/30

---
+++ 103_test_breakpoints.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Zeno on 2019/11/30.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum = 0;
+    int n = 10;
+    for(int i = 0;i < n;i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
