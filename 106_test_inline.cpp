0dbe406eaf23b6684366e0359ec8be60d16c38eb
YuzeZhang <zhangyuzechn@foxmail.com>
2019-12-01 20:05:22 +0800

    add

---
+++ 106_test_inline.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Zeno on 2019/12/2.
+//
+
+#include <iostream>
+using namespace std;
+
+inline int sum(int a,int b){
+    return a+b;
+}
+int main(){
+    int a = 1,b = 2;
+    sum(a,b);
+    return 0;
+}
