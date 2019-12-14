97b1bd44ef91a673d147aaf58fbadb73a494c89d
YuzeZhang <zhangyuzechn@foxmail.com>
2019-12-14 20:38:57 +0800

    add 2019/12/14

---
+++ 113_test_py.cpp
@@ -0,0 +1,29 @@
+//
+// Created by Zeno on 2019/12/12.
+//
+
+#include <iostream>
+using namespace std;
+
+class TestLib
+{
+public:
+    void display();
+    void display(int a);
+};
+void TestLib::display() {
+    cout<<"First display"<<endl;
+}
+
+void TestLib::display(int a) {
+    cout<<"Second display:"<<a<<endl;
+}
+extern "C" {
+TestLib obj;
+void display() {
+    obj.display();
+}
+void display_int() {
+    obj.display(2);
+}
+}
