2e541e4e1efc1ce4abd7a860c7cd44e964d4586e
YuzeZhang <zhangyuzechn@foxmail.com>
2020-02-27 11:09:31 +0800

    add

---
+++ 200_testMySQL.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Zeno on 2020/3/10.
+//
+#include <iostream>
+#include <mysql.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    MYSQL* _conn;// 表示和MySQL Server的一条连接
+    cout <<"test" << endl;
+    return 0;
+}
