63bfefbef76550d08b8c94aec373d873f6239f43
YuzeZhang <zhangyuzechn@foxmail.com>
2020-03-08 20:51:24 +0800

    add --2020/03/08

---
+++ 192_thread.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Zeno on 2020/3/7.
+//
+
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void threadHandle1(int time)
+{
+    // 让子线程睡眠两秒
+    std::this_thread::sleep_for(std::chrono::seconds(time));
+    cout << "hello thread1" << endl;
+}
+
+void threadHandle2(int time)
+{
+    // 让子线程睡眠time秒
+    std::this_thread::sleep_for(std::chrono::seconds(time));
+    cout << "hello thread2!" << endl;
+}
+
+int main()
+{
+    // 创建一个线程对象，传入一个线程函数，新线程就开始运行了
+    std::thread t1(threadHandle1, 2);
+    std::thread t2(threadHandle2, 3);
+
+    // 主线程等待子线程结束，主线程继续往下运行
+    t1.join();
+    t2.join();
+
+    // 把子线程设置为分离线程
+    //t1.detach();
+
+    cout << "main thread done" << endl;
+
+    // 主线程运行完成，查看如果当前进程还有未运行完成的子线程，进程就会异常终止
+    return 0;
+}
