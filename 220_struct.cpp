611536a18ad0e0da99c399126219a2cbb6e6c7ad
YuzeZhang <zhangyuzechn@foxmail.com>
2020-03-26 23:27:11 +0800

    add

---
+++ 220_struct.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Zeno on 2020/3/23.
+//
+
+
+#include <iostream>
+using namespace std;
+
+struct test
+{
+    char a;
+    int b;
+};
+
+int main()
+{
+    cout << sizeof(test) << endl;
+    return 0;
+}
